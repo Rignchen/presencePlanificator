0ebc52a0a20093a9ca529033f653057027060ec4
Rignchen <rinchendemort@gmail.com>
2023-12-15 11:52:37 +0100

    display seance data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import {Component} from "@angular/core";
 import {NgForOf} from "@angular/common";
 import {SeanceModule} from "../seance/seance.module";
+import {Seance} from "../seance/seance.model";
 
 @Component({
   standalone: true, selector: "seance-list", templateUrl: "./app.html", imports: [NgForOf, SeanceModule]
 })
 export class AppComponent {
-  seances: [string, string, number][] = [
-    ["Noël de Groupe", "16.12.2023", 1],
-    ["Séance de Troupe", "13.01.2024", 1],
-    ["Soirée médias", "13.01.2024", 1],
-    ["Camp d'été", "05.08.2024", 1],
-    ["Rangements du camp d'été", "15.08.2024", 1],
+  seances: Seance[] = [
+    new Seance("Noël de Groupe", "16.12.2023", 2),
+    new Seance("Séance de Troupe", "13.01.2024", 1),
+    new Seance("Soirée médias", "13.01.2024", 0),
+    new Seance("Camp d'été", "05.08.2024", 1),
+    new Seance("Rangements du camp d'été", "15.08.2024", 1),
   ]
 }
--- src/seance/seance.component.ts
+++ src/seance/seance.component.ts
@@ -1,8 +1,19 @@
 import {Component, Input} from "@angular/core";
+import {Seance} from "./seance.model";
 
 @Component({
   selector: "seance-item", templateUrl: "./seance.html", styleUrls: []
 })
 export class SeanceComponent {
-  @Input() seance: [string, string, number] = ["", "", 1];
+  @Input() seance: Seance = new Seance("", "", 0)
+
+  deleteSeance(id: number) {
+    const self = document.getElementById(String(id));
+    self?.remove();
+  }
+  setPresence(number: number) {
+    console.log(this.seance.value);
+    this.seance.value = number;
+    console.log(this.seance.value);
+  }
 }
