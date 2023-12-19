3955ba0c7ad7c75ef5e4e780b32a2ca6b4db926a
Rignchen <rinchendemort@gmail.com>
2023-12-19 09:52:34 +0100

    Implement seance type & date input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import {SeanceListComponent} from "./seance-list/seance-list.component";
 export const routes: Routes = [
   {
     path: "",
+    redirectTo: "seances",
+    pathMatch: "full"
+  },
+  {
+    path: "seances",
     component: SeanceListComponent
   }
 ];
--- src/app/seance-list/seance-list.component.ts
+++ src/app/seance-list/seance-list.component.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 import {SeanceElementComponent} from "../seance-element/seance-element.component";
 import {Seance} from "../seance-element/seance.model";
 import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-seance-list',
   standalone: true,
   imports: [
     SeanceElementComponent,
-    NgForOf
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './seance-list.component.html',
   styleUrl: './seance-list.component.css'
@@ -22,8 +24,31 @@ export class SeanceListComponent {
     new Seance("Rangements du camp d'été", "15.08.2024", 1),
   ]
   addSeance() {
-    let type = "Nouvelle séance";
-    let date = "01.01.2025";
+    // get wich input in the div newSeance is selected
+    let inputs: HTMLLabelElement|null = document.querySelector("#newSeance label:has(input:checked)");
+    let dateInput: HTMLInputElement|null = document.querySelector("#newSeanceDate");
+    if (inputs == null || dateInput == null) return;
+
+    // get the date
+    if (dateInput.value == "") return;
+    let date = dateInput.value;
+
+    //get the seance type
+    let type = inputs.textContent;
+    if (type == null) {return}
+    if (type == "Séance Spéciale") {
+      // if it's a special seance, the name is set by the user
+      let name: HTMLInputElement|null|undefined = inputs.parentElement?.querySelector("input[type=text]");
+      if (name != null) {
+        name.value = name.value.trim();
+        if (name.value != "") type = name.value;
+
+        // empty the values
+    name.value = "";}}
+    dateInput.value = "";
+    let pomme = inputs.querySelector("input");
+    if (pomme != null) pomme.checked = false;
+
     this.seances.push(new Seance(type, date, 1));
   }
 }
